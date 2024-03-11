74c5a1b5b4ae4abcd0f2532f4be443656eaa7688
amumuku <1527528140@qq.com>
2024-03-11 22:55:29 +0800

    Update 01-catchfacuet.py

--- 01-catchfacuet.py
+++ 01-catchfacuet.py
@@ -3,12 +3,17 @@ from eth_account import Account
 from loguru import logger
 
 from bera_tools import BeraChainTools
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+client_key = config['CLIENT_KEY']
 
 account = Account.create()
 logger.debug(f'address:{account.address}')
 logger.debug(f'key:{account.key.hex()}')
 # TODO 填写你的 YesCaptcha client key 或者2Captcha API Key 或者 ez-captcha ClientKey
-client_key = 'd5bef70c94be797fb54dfd645a5900a1a9b2a4d035135'
+# client_key = 'd5bef70c94be797fb54dfd645a5900a1a9b2a4d035135'
 # 使用yescaptcha solver googlev3
 bera = BeraChainTools(private_key=account.key, client_key=client_key,solver_provider='yescaptcha',rpc_url='https://rpc.ankr.com/berachain_testnet')
 # 使用2captcha solver googlev3
